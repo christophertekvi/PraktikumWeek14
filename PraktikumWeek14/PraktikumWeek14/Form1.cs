8e52c72a3bbcbd70f3935f2d8f289cb4a91c4bf3
Christopher tekvi <christophertekvi@hotmail.com>
2020-12-18 10:59:08 +0700

    Praktikum Week 14

--- PraktikumWeek14/PraktikumWeek14/Form1.cs
+++ PraktikumWeek14/PraktikumWeek14/Form1.cs
@@ -57,9 +57,33 @@ namespace PraktikumWeek14
 
             Array.Sort(arr1);
 
+            //char[] hurufbesar = new char[textBoxInput.Text.Length];
+            //char[] hurufkecil = new char[textBoxInput.Text.Length];
+            //char[] angka = new char[textBoxInput.Text.Length];
+            //char[] simbol = new char[textBoxInput.Text.Length];
 
+            //for (int i = 0; i < textBoxInput.Text.Length; i++)
+            //{
+            //    if (char.IsSymbol(arr1[i]) == true)
+            //    {
+            //        simbol[i] = arr1[i];
+            //    }
+            //    if (char.IsNumber(arr1[i]) == true)
+            //    {
+            //        angka[i] = arr1[i];
+            //    }
+            //    if (char.IsUpper(arr1[i]) == true)
+            //    {
+            //        hurufbesar[i] = arr1[i];
+            //    }
+            //    if (char.IsLower(arr1[i]) == true)
+            //    {
+            //        hurufkecil[i] = arr1[i];
+            //    }
+            //}
 
-            string output = new string(arr1);
+            string output = new string (arr1);
+
 
             labelOutput.Text = output;
         }
